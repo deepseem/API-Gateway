27a84f3671b8f0038f22a1476ff943e8ebac3537
deepseem <deepakmahto3039@gmail.com>
2026-05-11 00:43:30 +0530

    first commit-1

---
+++ src/main/java/com/example/api_gateway/security/JwtUtil.java
@@ -0,0 +1,36 @@
+package com.example.api_gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getKey() {
+
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+
+            Jwts.parser()
+                    .verifyWith(getKey())
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+        }
+    }
+}
